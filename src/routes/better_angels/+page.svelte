31977bee72e334e3b20b12ce6c08933dc2d3fbd6
CookieCrumb23 <5263987+CookieCrumb23@users.noreply.github.com>
2025-07-19 13:52:19 +0200

    feat: add content (still placeholders)

---
+++ src/routes/better_angels/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let data;
+	import Hero from '$lib/Hero.svelte';
+
+	const { image, title, text } = data;
+</script>
+
+<Hero {image} {title} {text} />
---
+++ src/routes/blades_in_the_dark/+page.ts
@@ -0,0 +1,7 @@
+export const load = () => {
+	return {
+		image: 'https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp',
+		title: 'Blades in the Dark',
+		text: 'This is bar content.'
+	};
+};
