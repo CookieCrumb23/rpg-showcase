edbcbf1d79b02c3c126c2373cb0de5f25c735c98
CookieCrumb23 <5263987+CookieCrumb23@users.noreply.github.com>
2025-07-19 13:23:07 +0200

    fix: update navigation links to include base path

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { navRoutes } from '$lib/navRoutes';
+	import { base } from '$app/paths';
 </script>
 
 <div class="absolute top-0 left-0 drawer z-auto">
@@ -7,7 +8,7 @@
 	<div class="p-3 drawer-content">
 		<!-- Page content here -->
 		<label for="my-drawer" class=" drawer-button">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
 				<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 			</svg>
 		</label>
@@ -16,7 +17,7 @@
 		<label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
 		<ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
 			{#each navRoutes as {href, label}}
-			<li><a href="{href}">{label}</a></li>
+			<li><a href="{base}/{href}">{label}</a></li>
 				{/each}
 		</ul>
 	</div>
